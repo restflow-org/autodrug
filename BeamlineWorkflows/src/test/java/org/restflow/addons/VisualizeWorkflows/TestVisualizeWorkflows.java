9a82b1be8817abdec09f47a2091b05b60dd48111
mcphscott <scottm@slac.stanford.edu>
2013-02-27 13:03:25 -0800

    new test for hierarchy tool to visualize AutoDrug workflow

--- BeamlineWorkflows/src/test/java/org/restflow/addons/VisualizeWorkflows/TestVisualizeWorkflows.java
+++ BeamlineWorkflows/src/test/java/org/restflow/addons/VisualizeWorkflows/TestVisualizeWorkflows.java
@@ -35,5 +35,26 @@ public class TestVisualizeWorkflows extends TestCase {
 			throw new Exception("stderr is not empty");
 		}
 	}
+	public void testVisualizeHierarchy() throws Exception {
+		String workspace = System.getenv("TEST_DATA");
+		
+        String file =  "classpath:tools/hierarchy.yaml";
+        
+		String runsDir = WorkflowContext.getUserTemporaryDirectoryPath() + "/testruns/";
+		String runDir = "VisualizeHierarchy_" + PortableIO.createTimeStampString();
+
+		System.out.println(runDir);
+		RestFlow.main( new String[] {"-base",runsDir, "-run" ,runDir,"-f",file,"-w","Visualize.WorkflowHierarchy" ,"-i","restflowFile=classpath:org/restflow/addons/AutoDrug/workflows/TestAutoDrug-OptionList.yaml","-i","workflowName=AutoDrug.A.Workflow"  });
+		
+		if ( new File(runsDir+runDir+"/_metadata/stderr.out").length() > 0) {
+			throw new Exception("stderr is not empty");
+		}
+	}
+
+
+
+
+
+
 
 }
