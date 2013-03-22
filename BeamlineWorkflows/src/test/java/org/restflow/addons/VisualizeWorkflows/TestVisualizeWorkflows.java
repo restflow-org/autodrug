e7f918a39a7e23de9d9eb6b724bf1ca9cc27ac84
mcphscott <scottm@slac.stanford.edu>
2013-03-21 18:43:03 -0700

    visualize webice workflow

--- BeamlineWorkflows/src/test/java/org/restflow/addons/VisualizeWorkflows/TestVisualizeWorkflows.java
+++ BeamlineWorkflows/src/test/java/org/restflow/addons/VisualizeWorkflows/TestVisualizeWorkflows.java
@@ -17,7 +17,7 @@ import org.restflow.util.StdoutRecorder;
 import org.restflow.util.TestUtilities;
 
 public class TestVisualizeWorkflows extends TestCase {
-	
+/*	
 	public void testVisualizeWorkflows() throws Exception {
 		String workspace = System.getenv("TEST_DATA");
 		
@@ -35,7 +35,8 @@ public class TestVisualizeWorkflows extends TestCase {
 			throw new Exception("stderr is not empty");
 		}
 	}
-	public void testVisualizeHierarchy() throws Exception {
+*/
+	public void testVisualizeAutoDrugHierarchy() throws Exception {
 		String workspace = System.getenv("TEST_DATA");
 		
         String file =  "classpath:tools/hierarchy.yaml";
@@ -51,7 +52,21 @@ public class TestVisualizeWorkflows extends TestCase {
 		}
 	}
 
+	public void testVisualizeWebiceProcessingHierarchy() throws Exception {
+		String workspace = System.getenv("TEST_DATA");
+		
+        String file =  "classpath:tools/hierarchy.yaml";
+        
+		String runsDir = WorkflowContext.getUserTemporaryDirectoryPath() + "/testruns/";
+		String runDir = "VisualizeWebiceHierarchy_" + PortableIO.createTimeStampString();
 
+		System.out.println(runDir);
+		RestFlow.main( new String[] {"-base",runsDir, "-run" ,runDir,"-f",file,"-w","Visualize.WorkflowHierarchy" ,"-i","restflowFile=classpath:org/restflow/addons/WebiceProcessing/workflow.yaml","-i","workflowName=WebiceProcessing.Img2Structure"  });
+		
+		if ( new File(runsDir+runDir+"/_metadata/stderr.out").length() > 0) {
+			throw new Exception("stderr is not empty");
+		}
+	}
 
 
 
