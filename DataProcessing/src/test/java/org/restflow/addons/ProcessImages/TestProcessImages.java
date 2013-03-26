e20631e8479f3adb973c5e02e6a55b377c4ca7aa
mcphscott <scottm@slac.stanford.edu>
2013-03-25 17:55:34 -0700

    add test for marccd images

--- DataProcessing/src/test/java/org/restflow/addons/ProcessImages/TestProcessImages.java
+++ DataProcessing/src/test/java/org/restflow/addons/ProcessImages/TestProcessImages.java
@@ -17,7 +17,7 @@ import org.restflow.util.TestUtilities;
 
 public class TestProcessImages extends TestCase {
 	
-	public void testProcessImages() throws Exception {
+	public void testProcessImagesCbf () throws Exception {
 		String testData = System.getenv("TEST_DATA");
 		
 		if (testData == null) throw new Exception("must set TEST_DATA environment variable");
@@ -40,5 +40,32 @@ public class TestProcessImages extends TestCase {
 			throw new Exception("stderr is not empty");
 		}
 	}
+
+	public void testProcessImagesMccd () throws Exception {
+		String testData = System.getenv("TEST_DATA");
+		
+		if (testData == null) throw new Exception("must set TEST_DATA environment variable");
+		
+		String infile = "target/test-classes/org/restflow/addons/ProcessImages/inputs/1/infile.yaml";
+        String file =  "classpath:org/restflow/addons/ProcessImages/workflows/TestProcessImages-A.yaml";
+        String firstImagePath = new File(testData + "/TestAddons/TestDataProcessing/2/A2_12_00001.mccd").getAbsolutePath();
+        String mrModel = "target/test-classes/org/restflow/addons/ProcessImages/inputs/2/2MBW.pdb";
+        
+        
+        //String runsDir = RestFlow.buildRunDirectoryContainer(new OptionSet());
+		
+		String runsDir = WorkflowContext.getUserTemporaryDirectoryPath() + "/testruns/";
+		String runDir = "ProcessImages" + PortableIO.createTimeStampString();
+
+		System.out.println(runDir);
+		RestFlow.main( new String[] {"-base",runsDir, "-run" ,runDir,"-f",file,"-w", "ProcessImages.Test.Workflow", "-infile", infile, "-i", "firstImagePath="+ firstImagePath,"-i","mr_model="+ mrModel});
+		
+		if ( new File(runsDir+runDir+"/_metadata/stderr.out").length() > 0) {
+			throw new Exception("stderr is not empty");
+		}
+	}
+
+
+
 
 }
